b07c8d3937d10b9e592cf55b34a2389131b6cfb8
Antoan Elenkov <antoanelenkov@abv.bg>
2015-03-26 03:13:45 +0200

    added the missing files

---
+++ HealthBuddy/HealthBuddy/Migrations/MyMigration.cs
@@ -0,0 +1,18 @@
+﻿namespace HealthBuddy.Migrations
+{
+    using HealthBuddy.Models;
+    using System.Collections.Generic;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+
+    public class MyMigration : DropCreateDatabaseIfModelChanges<HealthBuddyContext>
+    {
+        protected override void Seed(HealthBuddyContext context)
+        {
+            context.Desserts.Add(new Dessert("Tiramissu", 200m, 150m, 25, 25, 150, 56, new List<string> { "nuts" }));
+            context.Salads.Add(new Salad("TestSalad", 200m, 150m, 25, 25, 150, 56, new List<string> { "nuts" }));    
+            context.SaveChanges();
+
+        }
+    }
+}

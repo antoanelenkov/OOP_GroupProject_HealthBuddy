4c90faef4c60e58e584e80a84df96db96f137642
MariyaSteffanova <mariya.steffanova@gmail.com>
2015-04-02 11:32:11 +0300

    ,

---
+++ HealthBuddy/HealthBuddy/Interfaces/IMeal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthBuddy.Interfaces
+{
+    public interface IMeal
+    {
+        
+         string Name { get; set; }
+
+         decimal Calories { get; set; }
+
+         decimal Proteins { get; set; }
+
+         decimal Carbohydrates { get; set; }
+
+         decimal Fats { get; set; }
+
+         decimal Portion_Size { get; set; }
+
+         decimal Calories_Per_Portions { get; set; }
+
+         string Ingredients { get; set; }
+    }
+}

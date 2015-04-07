a3776f77bcc5e50fd4949ed75dbbd5ded7b7943a
MariyaSteffanova <mariya.steffanova@gmail.com>
2015-04-07 14:35:58 +0300

    History.cs added

---
+++ HealthBuddy/HealthBuddy/Models/History.cs
@@ -0,0 +1,11 @@
+﻿namespace HealthBuddy.Models
+{
+    using System;
+    using System.Collections.Generic;
+   public struct History
+    {
+      public  DateTime Date;
+       public ICollection<KeyValuePair<Meal, int>> Menu;
+
+    }
+}
